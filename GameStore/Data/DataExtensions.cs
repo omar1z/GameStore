11592e9e4ca233f87edfe0451fcb65ed0da92cdd
omar1z <omarzammar11@gmail.com>
2024-08-06 08:48:28 +0300

    Generating Database

---
+++ GameStore/Data/DataExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Data
+{
+    public static class DataExtensions
+    {
+        public static void MigrateDb(this WebApplication app)
+        {
+            // we cannot access DbContext directly we need a scope to interact with database
+            using var scope = app.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+            dbContext.Database.Migrate();
+        }
+    }
+}
